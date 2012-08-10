caec39ef8fb4499c1925892db767c4f032b37d84
Elio Capella Sánchez <elio.capella@kolobee.com>
2012-08-10 17:42:03 +0200

    trying to use ecma script 5 functions to reduce code

--- envy.js
+++ envy.js
@@ -1,29 +1,30 @@
+/*jslint node: true, indent: 4, maxlen: 80 */
+"use strict";
+
 // envy.js
 // Contains all the module logic.
 
 var envy = exports;
 
 // Load the .json file containing the 
-envy.load = function(filename) {
+envy.load = function (filename) {
 
-	// filenames are relative to the execution path
-	var config = require(process.env.PWD + '/' + filename);
+    // filenames are relative to the execution path
+    var config = require(process.env.PWD + '/' + filename);
 
-	// load default environment
-	envy.config = config[config.environment];
+    // load default environment
+    envy.config = config[config.environment];
 
-	// Overwrite properties
-	if (process.env.NODE_ENV && config[process.env.NODE_ENV]) {
-		for (var property in config[process.env.NODE_ENV]) {
-			envy.config[property] = config[process.env.NODE_ENV][property];
-		}
-	}
+    // Overwrite properties
+    if (process.env.NODE_ENV && config[process.env.NODE_ENV]) {
+        Object.defineProperties(envy.config, config[process.env.NODE_ENV]);
+    }
 
 };
 
 // Load the default configuration file if it exists
 try {
-	envy.load('config');
+    envy.load('config');
 } catch (e) {
-	// Ignore failure when loading default configuration file
+    // Ignore failure when loading default configuration file
 }
